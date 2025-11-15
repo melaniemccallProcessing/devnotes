3bee181b4c9564a5f125803c1d4717d0664e5bd0
melaniemcc <bluescript17@gmail.com>
2025-11-15 06:45:05 -0500

    registering router, bulma, and auth state listener

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import router from './router';
+import "bulma/css/bulma.css";
+import {auth, onAuthStateChanged} from "./firebase";
 
-createApp(App).mount('#app')
+let app;
+
+onAuthStateChanged(auth, () => {
+  // Ensure we mount only once auth state is resolved
+  if (!app) {
+    app = createApp(App);
+    app.use(router);
+    app.mount("#app");
+  }
+});
